0556472e05fa0c8f8e37bba50568ac0faf706560
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-06-15 19:25:02 +0530

    type checking in function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Button from "./Components/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>hello world!</>;
+  return (
+    <>
+      <div className="text-red-500">hello world</div>
+      <Button />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Button.tsx
@@ -0,0 +1,21 @@
+// function convertCurrency(amount: number, currency: string) {
+// ...
+// }
+
+// convertCurrency(100, "USD");
+
+const Button = () => {
+  //   let url = "https://google.com";
+  // url=8 <ERROR: not work>
+  return (
+    <>
+      <div>
+        <button className="text-white rounded-lg w-14 h-10 flex justify-center items-center bg-blue-600">
+          Button
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
