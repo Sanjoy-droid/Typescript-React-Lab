6b85983fd30b52b314c5f93e4f16daff1b60cb3b
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-06-19 21:50:19 +0530

    use as const in array

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,53 +1,20 @@
-// function convertCurrency(amount: number, currency: string) {
-// ...
-// }
+import { useRef, useState } from "react";
 
-import { useState } from "react";
-
-// convertCurrency(100, "USD");
-
-// const ExampleComponent: React:FC=()=>{}
-
-// type Color = "red" | "blue" | "green" | "yellow";
-
-//Interface
-
-// interface ButtonProps {
-//   type: "button" | "submit" | "reset";
-//   color: "red" | "blue" | "green" | "yellow";
-// }
-// interface SuperButtonProps extends ButtonProps {
-//   size: "small" | "medium" | "large";
-// }
-
-// Type Alias
-// type ButtonProps = {
-//   type: "button" | "submit" | "reset";
-//   color: "red" | "blue" | "green" | "yellow";
-// };
-
-// type SuperButtonProps = ButtonProps & {
-//   size: "small" | "medium" | "large";
-// };
-
-type User = {
-  name: string;
-  age: number;
-};
-
+const buttonTextOptions = [
+  "click me!",
+  "click me aganin",
+  "Click me one more again",
+] as const;
 export default function Button() {
-  const [count, setCount] = useState<number>(0);
-  const [text, setText] = useState("tackeis castle");
-
-  const [user, setUser] = useState<User | null>(null);
-
-  const name = user?.name;
   return (
     <>
       <div>
-        <button>CLICK ME</button>
+        <button>
+          {buttonTextOptions.map((option) => {
+            return option;
+          })}
+        </button>
         {/* {count} */}
-        {text}
       </div>
     </>
   );
